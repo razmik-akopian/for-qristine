31db52085228a66c58519d7d184e4ff8ab5115ef
Razmik Akopian <razmik.akopian.work@gmail.com>
2024-10-30 03:54:01 +0400

    add scripts

--- next.config.mjs
+++ next.config.mjs
@@ -5,6 +5,12 @@ const nextConfig = {
   experimental: {
     optimizePackageImports: ['@mantine/core', '@mantine/hooks'],
   },
+  output: 'export',
+  images: {
+    unoptimized: true,
+  },
+  basePath: '/for-qristine', // замените на имя вашего репозитория
 };
 
 export default nextConfig;
+// next.config.js
