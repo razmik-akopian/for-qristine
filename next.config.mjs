d1990adeefd2659a078f75339078d81762351d48
Razmik Akopian <razmik.akopian.work@gmail.com>
2024-10-30 04:24:07 +0400

    fix

--- next.config.mjs
+++ next.config.mjs
@@ -1,16 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  trailingSlash: true,
   swcMinify: true,
-  experimental: {
-    optimizePackageImports: ['@mantine/core', '@mantine/hooks'],
-  },
   output: 'export',
   images: {
     unoptimized: true,
   },
-  assetPrefix: '/for-qristine/',
 };
 
 export default nextConfig;
