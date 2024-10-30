5e84152202c3e1bd6f5f6614128dbb8fba00dd0d
Razmik Akopian <razmik.akopian.work@gmail.com>
2024-10-30 04:25:17 +0400

    Updates

--- theme.ts
+++
@@ -1,8 +0,0 @@
-'use client';
-
-import { createTheme } from '@mantine/core';
-
-export const theme = createTheme({
-  fontFamily: 'Manrope, sans-serif',
-  headings: { fontFamily: 'Manrope, sans-serif' },
-});
